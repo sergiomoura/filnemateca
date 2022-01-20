eaa9ee8d0ea763042bf2cf16b6a9fe5934b5c4ad
Sérgio Moura <smouracalmon@gmail.com>
2022-01-20 16:12:02 -0300

    Criação de rotas com express

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const FilmesServices = require('./services/FilmesServices');
+// Importação da biblioteca Express
+const express = require('express');
 
-console.log(FilmesServices.listar());
+// Criando um servidor
+const servidor = express();
+
+// Criando as rotas do servidor
+servidor.get('/', (req,res)=>{
+    console.log("Alguém fez uma requisição....");
+    res.send("Toma aí o que você me pediu...");
+})
+
+servidor.get('/filme', (req, res)=>{
+    console.log("Rota /filme foi acessada");
+    res.send("Filme: Miranha. Sinopse: Os cara piraram o cabeção...");
+})
+
+
+
+// Por o servidor para 'ouvir' as requisições
+servidor.listen(3000);
