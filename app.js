3442b80c8d7587e69c3acab4a08f8d540227b920
Sérgio Moura <smouracalmon@gmail.com>
2023-02-08 16:32:10 -0300

    Configurando pasta public e ejs

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 // Criando app
 const app = express();
 
+// Configurando pasta de arquivos estáticos acessíveis
+app.use(express.static('public'));
+
+// Configurando ejs como template engine
+app.set('view engine', 'ejs')
+
 // Importando e configurando router
 const router = require('./router');
 app.use(router);
