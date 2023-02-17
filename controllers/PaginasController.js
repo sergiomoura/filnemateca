fab4054f6ca6818f4eb92835e638b8aa34d835bc
Sérgio Moura <smouracalmon@gmail.com>
2023-02-17 10:18:45 -0300

    mostrando informações de um filme

--- controllers/PaginasController.js
+++ controllers/PaginasController.js
@@ -7,7 +7,15 @@ const PaginasController = {
         res.render("index.ejs", {filmes});
     },
     showFilme: (req, res) => {
-        res.render("filme.ejs");
+        // 1 - Capturar o id do filme desejado
+        const id = req.params.id;
+
+        // 2 - Capturar O filme desejado
+        const filmes = require('../database/filmes.json');
+        const filme = filmes.find(f => f.id == id);
+
+        // 3 - Renderizar a view filme.ejs, passando para ela o filme encontrado
+        res.render("filme.ejs", {filme});
     }
 }
 
